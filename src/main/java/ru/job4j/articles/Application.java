9e0c9aa1458e42dae187f56508ee1bc3b2f49503
ShamRail <rail1999g102r@mail.ru>
2021-07-12 21:13:34 +0500

    Создана базовая структура maven проекта

---
+++ src/main/java/ru/job4j/articles/Application.java
@@ -0,0 +1,14 @@
+package ru.job4j.articles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class.getSimpleName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world!");
+    }
+
+}
